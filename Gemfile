ecd4fc009df7e6380ef3de67b647b5a086bed13a
vincent <vincent@boohee.com>
2018-09-18 18:44:57 +0800

    升级依赖 sneakers 2.5.0, bunny ~> 2.6.4, amq-protocol ~> 2.1.0 <= 2.2.0

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'amq-protocol', '~> 2.1', '<= 2.2.0'
 # Specify your gem's dependencies in sneakers_packer.gemspec
 gemspec
--- test/sneakers_config.rb
+++ test/sneakers_config.rb
@@ -3,11 +3,14 @@ require 'sneakers'
 
 opts = {
   vhost: '/',
+  amqp: 'amqp://127.0.0.1:5672',
   exchange: 'sneakers',
   exchange_type: :direct,
-  workers: 2,
-  daemonize: true,
-  pid: "tmp/pids/sneakers.pid"
+  workers: 1,
+  daemonize: false,
+  pid: "tmp/pids/sneakers.pid",
+  daemon_process_name: "sneakers master [sneaker-test]",
+  worker_process_name: "sneakers worker %d [sneaker-test]"
 }
 
 Sneakers.configure(opts)
